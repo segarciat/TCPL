03889c3cd125d79a96789217273bf9517da02bcf
Sergio Garcia <segarciat314@gmail.com>
2024-01-11 10:22:33 -0500

    Initialize repo with hello example

---
+++ examples/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h> /* Includes information about standard library */
+
+
+/* Define a function named main that receives no argument values */
+main() /* Enclose statements of main within braces { } */
+{
+    /* main calls library function, 'printf', to print this sequence of characters: \n represents newline character */
+    printf("hello, world\n");
+}
