137ec4fd3720fdfe0829042996a63f75d635c4be
Sergio Garcia <segarciat314@gmail.com>
2024-01-22 17:11:37 -0500

    Update solution to 5-11 detab

--- ch5/exercises/11-entab-detab/detab.c
+++ ch5/exercises/11-entab-detab/detab.c
@@ -2,68 +2,45 @@
 #include <stdlib.h> /* atol */
 #define COLUMN_WIDTH 8
 
-int validate(int, char**);
-long handle_tabstop(long*, char*);
-char** reset(int *position, long *prevstop, long *tabwidth, char **tab_stop_list_start);
+int validate(int, char**, long[]);
 
 /* detab: converts tabs to the equivalent spaces to reach the next tab stop */
 int main(int argc, char *argv[])
 {
-    if (!validate(argc, argv++)) {
+    long widths[argc - 1];	/* tab stop widths computed from the list of tab stops */
+
+    if (!validate(argc, argv++, widths)) {
 	printf("Usage: provide a mononotically increasing list of positive integers, or no arguments at all\n");
 	return 1;
     }
 
-    int c;				/* current character read */
-    int  position;			/* current cursor position */
-    long tabwidth;			/* width of current tab stop */
-    long prevstop;			/* position of previous tab stop */
-    char **argvstart = argv;		/* pointer to position of first command-line argument, if any was provided */
+    int width_index;		/* index of next tab width to use */
+    int c;			/* current character being processed */
+    int position;		/* cursor position */
+    long tabwidth;		/* width from current tab stop to next one */
 
-    argv = reset(&position, &prevstop, &tabwidth, argvstart);
+    width_index = 0;
+    position = 0;
+    tabwidth = (argc == 1) ? COLUMN_WIDTH : widths[width_index++];
 
     while((c = getchar()) != EOF) {
 	if (c == '\n') {
 	    putchar(c);
-	    argv = reset(&position, &prevstop, &tabwidth, argvstart);
+	    position = 0;
+	    width_index = 0;
 	} else if (c == '\t') {
 	    while (position != tabwidth) {
 		putchar(' ');
 		position = position + 1;
 	    }
 	    position = 0;
-	    tabwidth = (argv == NULL) ? COLUMN_WIDTH: handle_tabstop(&prevstop, *argv++);
 	} else {
 	    putchar(c);
 	    position = (position + 1) % tabwidth;
-	    if (position == 0)
-	    	tabwidth = (argv == NULL) ? COLUMN_WIDTH: handle_tabstop(&prevstop, *argv++);
 	}
+
+	if (position == 0)
+	    tabwidth = (argc == 1 || width_index == argc - 1) ? COLUMN_WIDTH : widths[width_index++];
     }
     return 0;
-}
-
-/* handle_tabstop: updates value of prevstop to be the value of nextstop and returns new tabwidth value */
-long handle_tabstop(long *prevstop, char *nextstop)
-{
-    long newstop = atol(nextstop);
-    long tabwidth = newstop - *prevstop;
-    *prevstop = newstop;
-    return tabwidth;
-}
-
-/* reset: sets cursor position, value of prevtabstop, and tabwidth to initial values
- * returns the next position in tab stop list
- *
- * */
-char** reset(int *position, long *prevstop, long *tabwidth, char **tab_stop_list_start)
-{
-    *position = 0;
-    if (*tab_stop_list_start == NULL) {
-	*tabwidth = COLUMN_WIDTH;
-	return NULL;
-    } else {
-	*tabwidth = *prevstop = atol(*tab_stop_list_start);
-	return tab_stop_list_start + 1;
-    }
 }
--- ch5/exercises/11-entab-detab/validate.c
+++ ch5/exercises/11-entab-detab/validate.c
@@ -2,11 +2,11 @@
 #include <stdlib.h> /* strtol */
 
 /* validate: returns 1 if there are no command-line arguments, or it contains a monotonically increasing list
- * of natural nubmers
+ * of natural numbers and stores the corresponding widths in the given array, and returns 0 otherwise
  *
  * argc and argv is the number of arguments, and the arguments, passed to main
  * */
-int validate(int argc, char *argv[])
+int validate(int argc, char *argv[], long widths[])
 {
     if (argc == 1) {
         printf("using default tab: \n");
@@ -16,6 +16,7 @@ int validate(int argc, char *argv[])
         for (i = 1; i < argc; i++) {
             long val = strtol(argv[i], &endp, base);
             if (*endp == '\0' && val > prevtabstop) {
+		widths[i - 1] = val - prevtabstop;
                 prevtabstop = val;
             } else {
                 printf("invalid number: %s\n", endp);
